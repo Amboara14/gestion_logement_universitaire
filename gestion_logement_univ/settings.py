fe0cb6291a8727a1e2facf6193135835c9c1c03b
John <randriateddy4@gmail.com>
2026-08-14 15:16:32 +0200

    Init projet Django + config MySQL

--- gestion_logement_univ/settings.py
+++ gestion_logement_univ/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'gestion_logement_univ.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.MySQL',
+        'NAME': BASE_DIR / 'db.MySQL',
     }
 }
 
